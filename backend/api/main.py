0e7161085d5536cd04a665bc9f18772fd207a210
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-10-05 22:09:01 +0100

    Added CORS middleware Added Axios and example

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,11 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import users
 
 app = FastAPI()
 
 app.include_router(users.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,28 @@
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  const getMsg = async () => {
+    try {
+      const response = await api.get("/");
+
+      console.log(response.data);
+  
+      setMsg(response.data.message);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getMsg();
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>Home {msg}</div>
   );
 }
 
