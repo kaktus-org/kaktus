1864952c53fdca889a7ba634f4ead354af4b9a75
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-10-07 12:02:39 +0100

    WIP: set up crude database

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,17 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
 from .routers import users
+
+from backend.db.models import users as users_model
+from backend.db.crud import user as user_crud
+from backend.db.schemas import user as user_schema
+
+from backend.db.database import SessionLocal, engine
+
+users_model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -20,6 +29,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 async def root():
