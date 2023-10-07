f1c5ea69815390797fb490f77cdb1cb4adb179f2
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-10-07 13:38:37 +0100

    Finish adding basic postgres support

--- backend/api/routers/users.py
+++ backend/api/routers/users.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from api.main import get_db
+from db.crud import user
 
 router = APIRouter(
     prefix="/users",
@@ -12,7 +13,8 @@ fake_users = {1: "Harley", 2: "Lewis"}
 
 @router.get("/")
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return "All users here.."
+    users = user.get_users(db, skip=skip, limit=limit)
+    return users
 
 @router.get("/{user_id}")
 async def read_user(user_id: str):
