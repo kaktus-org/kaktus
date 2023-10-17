99c2a85d706405869b4cf0049b4b5852515c674d
Harley <50149947+harleyreevesmartin@users.noreply.github.com>
2023-10-17 21:06:11 +0100

    Basic Dockerisation
    * Rename poetry.script 'start -> start-api'
    
    * Set up basic docker compose

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-SQLALCHEMY_DATABASE_URL: str = "postgresql://admin:admin@localhost/db"
+SQLALCHEMY_DATABASE_URL: str = "postgresql://admin:admin@yampacked-database-1/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
