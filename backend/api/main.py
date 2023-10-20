b3a19f1c395dcb7bfa008beed57987539398a937
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-10-20 23:49:16 +0100

    Add basic logger for api module

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,8 @@
-import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import logging
 from sqlalchemy.orm import Session
+import uvicorn
 
 def get_db():
     db = SessionLocal()
@@ -11,10 +12,16 @@ def get_db():
         db.close()
 
 from .routers import users
-
 from db.models import users as users_model
-
 from db.database import SessionLocal, engine
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="api.log"
+)
+
+logger = logging.getLogger(__name__)
 
 users_model.Base.metadata.create_all(bind=engine)
 
@@ -35,13 +42,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
-
 @app.get("/")
 async def root():
     return {"message": "yurr"}
 
 def start():
+    logger.info("Starting application on 0.0.0.0:8000")
     uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
-
