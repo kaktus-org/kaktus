acb35e6f41b0be88f8e763123427d868f2d1cbf5
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-10-21 00:06:09 +0100

    Add logging example to database module

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,7 +1,16 @@
+import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
 
 SQLALCHEMY_DATABASE_URL: str = "postgresql://admin:admin@yampacked-database-1/db"
 
@@ -9,6 +18,7 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 
+logger.info(f"Creating local session '{engine.url}'")
 SessionLocal: Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
