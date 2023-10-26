b6e8a0059f379250937b09fd79b7cb96bd07aa50
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-10-26 21:54:03 +0100

    plaid-link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,13 @@
 import "./App.css";
-import api from "./api/axiosConfig";
-import { useState, useEffect } from "react";
+import { Link } from "./api/openBanking";
 
 function App() {
-  const [msg, setMsg] = useState("");
-
-  const getMsg = async () => {
-    try {
-      const response = await api.get("/");
-
-      console.log(response.data);
-
-      setMsg(response.data.message);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    getMsg();
-  }, []);
-
-  return <div className="text-3xl font-bold underline">Home {msg}</div>;
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">Home</div>
+      <Link />
+    </div>
+  );
 }
 
 export default App;
