5ec7af07f2ee8472674efbae910bf5a9bb71e130
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-11-08 20:39:15 +0000

    added router dom

---
+++ frontend/src/screens/home/Home.tsx
@@ -0,0 +1,10 @@
+import { PairBankButton } from "components/openBanking/OpenBanking";
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">Home</div>
+      <PairBankButton />;
+    </div>
+  );
+};
