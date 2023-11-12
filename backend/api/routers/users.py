1a3317ab085b5b4018c4a71147d20a4d300ab61e
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-11-12 17:23:21 +0000

    feat: Add basic frontend form that allows registration of new users

--- backend/api/routers/users.py
+++ backend/api/routers/users.py
@@ -1,41 +1,35 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-<<<<<<< HEAD
 from utils.database_utils import get_db
-from db.crud import user
-=======
-from api.main import get_db
-from db.crud import user as user_crud
-from db.models import users
-from db.schemas import user as user_schema
->>>>>>> 8369c39 (feat: Add backend support for creating a user)
+from db.crud.user import create_user
+from db.crud.user import get_user, get_users, get_user_by_email
+from db.schemas.user import User, UserCreate
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
 
-fake_users = {1: "Harley", 2: "Lewis"}
-
 
 @router.get("/")
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = user_crud.get_users(db, skip=skip, limit=limit)
+    users = get_users(db=db, skip=skip, limit=limit)
     return users
 
 
-@router.get("/{user_id}")
-async def read_user(user_id: str):
-    user_id_int = int(user_id)
-    if user_id_int not in fake_users:
-        raise HTTPException(status_code=404, detail="User not found")
-    return f"Name is {fake_users[user_id_int]}"
-
-@router.post("/users/", response_model=user_schema.User)
-def create_user(user: user_schema.UserCreate, db: Session=Depends(get_db)):
-    db_user: user_schema.User = user_crud.get_user_by_email(db, email=user.email)
+@router.post("/", response_model=User)
+def create_user(user: UserCreate, db: Session=Depends(get_db)):
+    db_user: User = get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return user_crud.create_user(db=db, user=user)
+    return create_user(db=db, user=user)
+
+
+@router.get("/{user_id}")
+async def read_user(user_id: int, db: Session=Depends(get_db)):
+    db_user = get_user(db, user_id=user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
--- backend/db/crud/user.py
+++ backend/db/crud/user.py
@@ -1,25 +1,26 @@
 from sqlalchemy.orm import Session
 
-from db.models import users as user_model
-from db.schemas import user as user_schema
+from db.models.users import User
+from db.schemas.user import UserCreate
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(user_model.User).filter(user_model.User.id == user_id).first()
+    return db.query(User).filter(User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(user_model.User).filter(user_model.User.email == email).first()
+    return db.query(User).filter(User.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(user_model.User).offset(skip).limit(limit).all()
+    return db.query(User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: user_schema.UserCreate):
+def create_user(db: Session, user: UserCreate):
     not_a_real_password = user.password + "nothashed" # TODO: obviously insecure, make this secure.
-    db_user = user_model.User(email=user.email, hashed_password=not_a_real_password, is_active=True)
+    db_user = User(email=user.email, hashed_password=not_a_real_password, is_active=True)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
