e56189f19675acc04be9283d60bcf3d7d1cfc900
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-11-12 23:55:08 +0000

    feat: Add autofix arguments to linting scripts

--- scripts/run-lint-backend.sh
+++ scripts/run-lint-backend.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 set pipefail
 
+USAGE="Usage: $(basename $0) [-f | --fix]"
+
+FIX=""
+
+for arg in "$@"; do
+    case "$arg" in
+        --help|-h)      echo "${USAGE}"; exit 0;;
+        --fix|-f)       FIX="--fix";;
+        *)              echo -e "unknown option $arg\n$USAGE" >&2; exit 1;;
+    esac
+done
+
+
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 repo_dir="$(dirname "${script_dir}")"
 
 echo "Running Python Linting..."
+
+if [ ! -z "${FIX}" ]; then
+    echo "'--fix' specified, fixing, this will make changes!"
+    autopep8 "${repo_dir}/backend/" -r --in-place || echo "Autofix failed... Do you hve autopep8 installed and on path? Continuing anyway."
+fi
 
 echo "Checking for critical errors"
 echo "Running: flake8 ./backend --count --select=E9,F63,F7,F82 --show-source --statistics"
--- scripts/run-lint-frontend.sh
+++ scripts/run-lint-frontend.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
+
 set -eo pipefail
+USAGE="Usage: $(basename $0) [-f | --fix]"
+
+FIX=""
+
+for arg in "$@"; do
+    case "$arg" in
+        --help|-h)      echo "${USAGE}"; exit 0;;
+        --fix|-f)       FIX="--fix";;
+        *)              echo -e "unknown option $arg\n$USAGE" >&2; exit 1;;
+    esac
+done
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 repo_dir="$(dirname "${script_dir}")"
 
 echo "Running TypeScript Linting..."
 pushd "${repo_dir}/frontend"
+
+if [ ! -z "${FIX}" ]; then
+    echo "'--fix' specified, fixing, this will make changes!"
+    yarn fix
+fi
+
 yarn lint
