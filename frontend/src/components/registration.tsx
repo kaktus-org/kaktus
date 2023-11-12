efab8cff24ec046f400fd9d1a91fed1b90d07a77
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-11-12 17:23:21 +0000

    feat: Add basic frontend form that allows registration of new users

---
+++ frontend/src/components/registration.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+interface UserRegistrationData {
+    email: string;
+    password: string;
+}
+
+const registerUser = async (userData: UserRegistrationData) => {
+    try {
+        const response = await axios.post("http://localhost:8000/users/users/", userData);
+        console.log("User registered:", response.data);
+    } catch (error) { 
+        console.error("Error registering user:", error)
+        // TODO: handle the error better, display error to user.
+    }
+}
+
+const RegistrationForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        await registerUser({ email, password });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+            />
+            <input 
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+            />
+            <button type="submit">Register</button>
+        </form>
+    )
+};
+
+export default RegistrationForm;
--- frontend/src/screens/home/Home.tsx
+++ frontend/src/screens/home/Home.tsx
@@ -1,10 +1,12 @@
 import { PairBankButton } from "components/openBanking/OpenBanking";
+import RegistrationForm from "components/registration";
 
 export const Home = () => {
   return (
     <div>
       <div className="text-3xl font-bold underline">Home</div>
-      <PairBankButton />;
+      <PairBankButton />
+      <RegistrationForm />
     </div>
   );
 };
