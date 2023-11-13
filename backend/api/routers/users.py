5d844c3db7ce9ba3afb88866fd616030bc5e7dce
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-11-13 23:14:52 +0000

    lint: please linter

--- backend/api/routers/users.py
+++ backend/api/routers/users.py
@@ -19,7 +19,7 @@ async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 
 
 @router.post("/", response_model=User)
-def create_user(user: UserCreate, db: Session=Depends(get_db)):
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user: User = get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -27,9 +27,8 @@ def create_user(user: UserCreate, db: Session=Depends(get_db)):
 
 
 @router.get("/{user_id}")
-async def read_user(user_id: int, db: Session=Depends(get_db)):
+async def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = get_user(db, user_id=user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
