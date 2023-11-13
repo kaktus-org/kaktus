0247fce216ecd42fb6c63d3006a48f370cb1720d
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-11-13 00:00:09 +0000

    fix: account for pipefail argument in argument parsing

--- backend/db/crud/user.py
+++ backend/db/crud/user.py
@@ -17,10 +17,8 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: user_schema.UserCreate):
-    # TODO: obviously insecure, make this secure.
-    not_a_real_password = user.password + "nothashed"
-    db_user = user_model.User(
-        email=user_model.User.email, hashed_password=not_a_real_password)
+    not_a_real_password = user.password + "nothashed" # TODO: obviously insecure, make this secure.
+    db_user = user_model.User(email=user.email, hashed_password=not_a_real_password, is_active=True)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
