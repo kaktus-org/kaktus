b51571e55eb9cb1344bfaa798c4503a0533dddd2
Harley Reeves-Martin <harleyreevesmartin@gmail.com>
2023-11-15 20:14:30 +0000

    feat: add functional registration page, responsive + hooked up to backend

--- frontend/src/screens/home/Home.tsx
+++ frontend/src/screens/home/Home.tsx
@@ -1,12 +1,10 @@
 import { PairBankButton } from "components/openBanking/OpenBanking";
-import RegistrationForm from "components/registration";
 
 export const Home = () => {
   return (
     <div>
       <div className="text-3xl font-bold underline">Home</div>
       <PairBankButton />
-      <RegistrationForm />
     </div>
   );
 };
