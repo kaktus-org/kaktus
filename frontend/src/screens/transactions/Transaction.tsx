eb6eb92fbc9579683a0a818704bd13ec16bb5858
Lewis-McKenzie <mckenzielewis39@gmail.com>
2024-01-23 21:09:42 +0000

    transactions update

---
+++ frontend/src/screens/transactions/Transaction.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import api from "api";
+
+interface TransactionProps {
+    transactionData: TransactionData
+}
+
+export interface TransactionData {
+    account_id: string,
+    account_owner: string,
+    amount: number,
+    authorized_date: string,
+    authorized_datetime: string,
+    category: string[],
+    category_id: string,
+    check_number: string,
+    counterparties: any[],
+    date: string,
+    datetime: string,
+    iso_currency_code: string,
+    location: any,
+    logo_url: string,
+    merchant_entity_id: string,
+    merchant_name: string,
+    name: string,
+    payment_channel: string,
+    payment_meta: any,
+    pending: boolean,
+    pending_transaction_id: string,
+    personal_finance_category: any,
+    personal_finance_category_icon_url: string,
+    transaction_code: string,
+    transaction_id: string,
+    transaction_type: string,
+    unofficial_currency_code: string,
+    website: string
+}
+
+const Transaction = ({ transactionData }: TransactionProps) => {
+
+    return (
+        <div>{transactionData.amount}
+        </div>
+    );
+};
+
+export default Transaction;
+
--- frontend/src/screens/transactions/Transactions.tsx
+++ frontend/src/screens/transactions/Transactions.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
+import Transaction from "./Transaction";
+import { TransactionData } from "./Transaction";
 import api from "api";
 
+interface AccountData {
+    added: any[]
+}
+
+
 interface Transactions {
-    data: JSON
+    data: AccountData[]
 }
 
 const TransactionsPage = () => {
-    const [transactionData, setTransactionData] = useState('');
+    const [transactionData, setTransactionData] = useState<AccountData[]>([]);
     const [error, setError] = useState('');
 
     const fetchTransactions = async () => {
         try {
             const response: Transactions = await api.get('banking/transactions', true);
-            setTransactionData(JSON.stringify(response.data, null, 2));
+            console.log(response);
+            setTransactionData(response.data);
         } catch (err) {
             console.error('Error fetching transactions:', err);
             setError('Failed to retrieve transactions. Please make sure you are logged in.');
@@ -24,7 +32,18 @@ const TransactionsPage = () => {
             <h1>Transactions</h1>
             <button onClick={fetchTransactions}>Fetch Transactions</button>
             {error && <div>Error: {error}</div>}
-            {transactionData && <pre>{transactionData}</pre>} {/* Display the formatted JSON data */}
+            {transactionData &&
+                <ul>
+                    {transactionData.map((account) => {
+                        return <ul>
+                            {account.added.map((transaction: TransactionData) => {
+                                return <Transaction transactionData={transaction} />
+                            })}
+                        </ul>
+                    })}
+                    {/* <pre>{JSON.stringify(transactionData, null, 2)}</pre> */}
+                </ul>
+            }
         </div>
     );
 };
