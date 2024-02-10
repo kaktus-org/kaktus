9e8bcd3e78bbd63ab508d4d830f4cb1c47772481
Lewis-McKenzie <mckenzielewis39@gmail.com>
2024-02-10 16:53:56 +0000

    live-db-integration
    update db config to allow live db

--- backend/utils/security/config.py
+++ backend/utils/security/config.py
@@ -10,8 +10,5 @@ class AuthConfig(Config):
         self.csrf_algorithm = Config.get_string("CSRF_ALGORITHM", "HS256")
         self.access_token_expire_mins = Config.get_int("ACCESS_TOKEN_EXPIRE_MINS", default=30)
 
-    def get_db_url(self):
-        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
-
 
 auth_config: AuthConfig = AuthConfig()
